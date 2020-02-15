13dfa7e6a126ec699f93cd25d628ffdf6df2fa8f
Andrew Locke <alocke12992@gmail.com>
2020-02-15 14:48:47 -0800

    Adds profile btn to create new donation

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import React, {Fragment} from 'react';
-import {Route, Switch} from 'react-router-dom';
+import {Route, Switch, Redirect } from 'react-router-dom';
 import AuthRoute from './components/AuthRoute';
+import Navbar from './components/Navbar';
 import Profile from './views/Profile';
 import MapView from './views/Map';
 import Login from './views/Login'
 
 const App = () => (
-  <Switch>
-    <AuthRoute exact path='/' component={Profile} />
-    <AuthRoute exact path='/map' component={MapView} />
-    <Route exact path='/login' component={Login} />
-  </Switch>
+  <>
+    <Navbar />
+    <Switch>
+      <AuthRoute exact path='/' component={() => <Redirect to="/profile" />} />
+      <AuthRoute exact path='/profile' component={Profile} />
+      <AuthRoute exact path='/map' component={MapView} />
+      <Route exact path='/login' component={Login} />
+    </Switch>
+  </>
 )
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Link, useHistory } from 'react-router-dom';
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  height: 75px;
+  border-bottom: solid 1px #333;
+  align-items: center;
+
+
+  .links {
+    display: flex;
+    align-items: center;
+
+    a { 
+      margin-right: 5px;
+    }
+  }
+`;
+
+const Navbar = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const history = useHistory();
+
+  const handleLogout = () => {
+    localStorage.removeItem('user');
+    setLoggedIn(false);
+    history.push('/');
+  }
+
+  useEffect(() => {
+    if (localStorage.getItem('user')) {
+      setLoggedIn(true);
+    } else {
+      setLoggedIn(false);
+    }
+  }, [history])
+  return (
+  <Container>
+    <div>Swoosh</div>
+    <div className="links">
+      {
+        loggedIn && (
+          <>
+            <Link to="/profile">Profile</Link>
+            <Link to="/map">Map</Link>
+            <button onClick={handleLogout}>Log out</button>
+          </>
+      )}
+    </div>
+  </Container>
+)}
+
+export default Navbar;
