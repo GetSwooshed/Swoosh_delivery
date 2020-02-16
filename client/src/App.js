d2e46c192d35e5158c8ab00b8da893b7779f30a9
Karin Hsu <khsu0720@gmail.com>
2020-02-15 17:39:56 -0800

    mock data markers on map

--- client/src/App.js
+++ client/src/App.js
@@ -5,16 +5,18 @@ import Navbar from './components/Navbar';
 import Profile from './views/Profile';
 import MapView from './views/Map';
 import Login from './views/Login'
+import Marker from './views/Marker';
 
 const App = () => (
   <>
-    <Navbar />
+    {/* <Navbar />
     <Switch>
       <AuthRoute exact path='/' component={() => <Redirect to="/profile" />} />
       <AuthRoute exact path='/profile' component={Profile} />
       <AuthRoute exact path='/map' component={MapView} />
       <Route exact path='/login' component={Login} />
-    </Switch>
+    </Switch> */}
+    <Marker />
   </>
 )
 
