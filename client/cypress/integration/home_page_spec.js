ab3c5aadb305990ff2a1c5f3c0de12916dc7db5e
Karin Hsu <khsu0720@gmail.com>
2020-02-26 15:47:21 -0800

    implemented home page testing, still need to add tests for existing user login validation

--- client/cypress/integration/home_page_spec.js
+++ client/cypress/integration/home_page_spec.js
@@ -2,4 +2,23 @@ describe('The Home Page', function() {
   it('successfully loads', function() {
     cy.visit('/') // change URL to match your dev URL
   })
+
+  it('successfully logs in using create new user', function() {
+    cy.contains('Don\'t have an account').click();
+
+    // cy.url().should('include', '/newUrl');
+
+    cy.get('.email_input')
+    .type('example@example.com')
+
+    cy.get('.pw_input')
+    .type('test')
+
+    cy.get('.login_submit_btn').click();
+  })
+
+  //add test that sends req to server to check for email and password validation
+  // it('successfully authorizes email/pw for old user', function() {
+
+  // })
 })
